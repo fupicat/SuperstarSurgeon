7d58dbf352f5f0af87414f5d0c1ad7599814f68b
Fupicat <fupicat@gmail.com>
2026-02-26 17:59:39 -0300

    fixes para web

--- endings/ending.gd
+++ endings/ending.gd
@@ -1,4 +1,10 @@
 extends Control
 
+func _ready() -> void:
+	$AudioStreamPlayer.play()
+	$AudioStreamPlayer.finished.connect(func():
+		$AudioStreamPlayer.play()
+	)
+
 func back_to_menu(_anim_name):
-    get_tree().change_scene_to_file("res://menu/menu.tscn")
+	get_tree().change_scene_to_file("res://menu/menu.tscn")
